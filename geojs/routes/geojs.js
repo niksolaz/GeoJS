9e2b93f3754e9b1fc7bd387815fd8165e733f217
niksolaz <solazzo.nicola@gmail.com>
2015-04-18 15:48:06 +0200

    add method post

--- geojs/routes/geojs.js
+++ geojs/routes/geojs.js
@@ -6,4 +6,7 @@ router.get('/',function(req,res,next){
 	res.render('geojs', { name: 'username' , city: 'city'});
 });
 
+router.post('/',function(req,res,next){
+	res.send('Data Sent');
+});
 module.exports = router;
