4c53f81d5dbb4f6a17eb44f013ab9d6c307b159c
niksolaz <solazzo.nicola@gmail.com>
2015-04-20 22:07:17 +0200

    add db

--- geojs/models/location.js
+++ geojs/models/location.js
@@ -15,7 +15,20 @@ db.once('open',function(callback){
     console.log('Mongodb is connected');
 }); 
 
-module.exports.create = function(city){
+module.exports.methods = function(city){
     var cityLocation = new Locations({city:city})
-    console.log(cityLocation.city)    
+    console.log(cityLocation.city)
+
+    cityLocation.save(function(err,cityLocation){
+        if(err) return console.log('The object has been added to the db ...');
+    }); 
+
+    cityLocation.find(function(err,cityLocation){
+        if(err) return console.log('The object has been finded to the db...');
+
+    cityLocation.update(function(err,cityLocation){
+        if(err) return console.log('The object has been updated to the db...');
+
+    cityLocation.delete(function(err,cityLocation){
+        if(err) return console.log('The object has been deleted to the db...'); 
 }
