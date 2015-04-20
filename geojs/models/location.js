b6e7a4827b3851eeae8a1905cb761c5d92db1dc9
niksolaz <solazzo.nicola@gmail.com>
2015-04-20 21:31:46 +0200

    add db

--- geojs/models/location.js
+++ geojs/models/location.js
@@ -6,14 +6,18 @@ mongoose.connect('mongodb://localhost:27017/mydb');
 var locationSchema = mongoose.Schema({
         city : String
     })
-//Model//
-var Locations = mongoose.model('location',locationSchema)
 
-var cityLocation = new Locations({city:'milano'})
-console.log(cityLocation.city)
 
 var db = mongoose.connection;
 db.on('error',console.error.bind(console,'connection error'));
 db.once('open',function(callback){
     console.log('Mongodb is connected');
 }); 
+
+module.exports.create = function(city){
+    //Model//
+    var Locations = mongoose.model('location',locationSchema)
+
+    var cityLocation = new Locations({city:'milano'})
+    console.log(cityLocation.city)    
+}
