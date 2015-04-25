63cd2df46b9fbab34d23ebec76bc26a3515e5c00
niksolaz <solazzo.nicola@gmail.com>
2015-04-25 16:43:45 +0200

    router post

--- geojs/routes/geojs.js
+++ geojs/routes/geojs.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var LocationModel = require('../models/location');
 
-var LocationRequest = new LocationModel.create();
 /* GET GeoJS page */
 router.get('/',function(req,res,next){
 	res.render('geojs', { city:'city'});
@@ -10,10 +9,10 @@ router.get('/',function(req,res,next){
 
 /* POST GeoJs page */
 router.post('/',function(req,res,next){
-	LocationRequest.create = req.body.city;
-	res.send('City of '+ LocationRequest );
+	var LocationModel = create(req.body.city);
+	res.send('City of '+ LocationModel );
 });
-console.log(LocationRequest);
+console.log(LocationModel);
 
 module.exports = router;
 
