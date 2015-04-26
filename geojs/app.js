f046fb7db1a445d7963c9b81b953c64069051415
niksolaz <solazzo.nicola@gmail.com>
2015-04-26 16:01:21 +0200

    routes GET /_id

--- geojs/app.js
+++ geojs/app.js
@@ -35,6 +35,7 @@ app.use('/', routes);
 app.use('/users', users);
 app.use('/geojs',geojs);
 
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
--- geojs/routes/geojs.js
+++ geojs/routes/geojs.js
@@ -7,6 +7,11 @@ router.get('/',function(req,res,next){
 	res.render('geojs', { city:'city'});
 });
 
+/* GET _id */
+router.get('/_id',function(res,req,next){
+	res.send('_id');
+});
+
 /* POST GeoJs page */
 router.post('/',function(req,res,next){
 	LocationModel.create(req.body.city);
